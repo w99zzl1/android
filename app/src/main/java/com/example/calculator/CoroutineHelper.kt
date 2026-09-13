c82ee2fc4964efcb2d9bb8427f6e3f2ced4c7d27
Samsonov <marimasomsonova1986@gmail.com>
2026-09-13 14:35:53 +0300

    Fix launch parameter name from action to block

--- app/src/main/java/com/example/calculator/CoroutineHelper.kt
+++ app/src/main/java/com/example/calculator/CoroutineHelper.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.*
 object CoroutineHelper {
     private val ioScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     fun runInIO(action: suspend () -> Unit): Job {
-        return ioScope.launch(action = action)
+        return ioScope.launch(block = action)
     }
 }

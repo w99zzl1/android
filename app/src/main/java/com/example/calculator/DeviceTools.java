13bbac1398bb48eba36f95890fb8d7f8eaaf3d25
Samsonov <marimasomsonova1986@gmail.com>
2026-09-14 16:55:01 +0300

    Fix AlarmClock import (android.provider.AlarmClock)

--- app/src/main/java/com/example/calculator/DeviceTools.java
+++ app/src/main/java/com/example/calculator/DeviceTools.java
@@ -1,6 +1,6 @@
 package com.example.calculator;
 
-import android.app.AlarmClock;
+import android.provider.AlarmClock;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;

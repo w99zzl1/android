a1b084ac761dd23d2e74172b281c26760de823e8
Samsonov <marimasomsonova1986@gmail.com>
2026-09-14 14:24:44 +0300

    Fix ViewGroup import for MessagesAdapter

--- app/src/main/java/com/example/calculator/MessagesAdapter.java
+++ app/src/main/java/com/example/calculator/MessagesAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
